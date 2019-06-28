44ffd7137494a5bf923853b1d47d23a13d0c141c
manishparui <manishparui8@gmail.com>
2019-06-28 10:20:05 +0530

    First Commit

---
+++ app/src/main/java/manish/webviewtemplate/MainActivity.java
@@ -0,0 +1,25 @@
+package manish.webviewtemplate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView xWebView;
+    String url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        url = "http://madmanish.com";
+        xWebView = (WebView) findViewById(R.id.xWebView);
+
+        xWebView.getSettings().setJavaScriptEnabled(true);
+        xWebView.loadUrl(url);
+        xWebView.setWebViewClient(new WebViewClient());
+    }
+}
